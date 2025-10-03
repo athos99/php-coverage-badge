751ea667619db70ba4d753ee21328353b627ab7a
Bobillier Michel <michel.bobillier@etat.ge.ch>
2025-10-03 17:02:57 +0200

    feat: build

--- src/Build.php
+++ src/Build.php
@@ -43,6 +43,33 @@ class Build
         return $metric;
     }
 
+    private function buildBadge(int $name, int $covered, int $valid)
+    {
+        $coverage = ($valid === 0) ? 100.0 : number_format(($covered * 100) / $valid, 2, '.');
+        if ($coverage < 40) {
+            $color = '#e05d44';  // Red
+        } elseif ($coverage < 60) {
+            $color = '#fe7d37';  // Orange
+        } elseif ($coverage < 75) {
+            $color = '#dfb317';  // Yellow
+        } elseif ($coverage < 90) {
+            $color = '#a4a61d';  // Yellow-Green
+        } elseif ($coverage < 95) {
+            $color = '#97CA00';  // Green
+        } elseif ($coverage <= 100) {
+            $color = '#4c1';     // Bright Green
+        } else {
+            $color = '#a4a61d';      // Default Gray
+        }
+
+        $template = file_get_contents(__DIR__ . 'flat.svg');
+        $template = str_replace('{{ name }}', $name, $template);
+        $template = str_replace('{{ total }}', $coverage, $template);
+        $template = str_replace('{{ color }}', $color, $template);
+        file_put_contents($outputFile, $template);
+    }
+
+
 
 
     public function test()
