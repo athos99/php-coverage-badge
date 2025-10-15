cc7beb88fb423fd8bc1e3e61cb782a261cbb7f90
Bobillier Michel <michel.bobillier@etat.ge.ch>
2025-10-15 13:56:33 +0200

    Chore: update badge

--- Build.php
+++ Build.php
@@ -1,4 +1,8 @@
 <?php
+
+include_once 'Dict.php';
+
+
 class Metric
 {
     public int $branchesCovered;
@@ -44,17 +48,71 @@ class Build
     }
 
 
-
-    private function buildBadge(string $name, int $width, int $covered, int $valid, float $limit, string $outputFile): void
+    private function  calculateTextLength110(string $s)
     {
-        $coverage = ($valid === 0) ? 100.0 : number_format(($covered * 100) / $valid, 1, '.');
+
+        if ($s == null || strlen($s) == 0) {
+            return 0;
+        }
+        $total = 0;
+        $len = mb_strlen($s);
+        for ($i = 0; $i < $len; $i++) {
+            $c = mb_substr($s, $i, 1);
+            if (array_key_exists($c, Dict::$defaultWidths["character-lengths"])) {
+                $total += Dict::$defaultWidths["character-lengths"][$c];
+            } else {
+                $total += Dict::$defaultWidths["mean-character-length"];
+            }
+            if ($i > 0) {
+                $pair = mb_substr($s, $i - 1, 2, 'UTF-8');
+                if (array_key_exists($pair, Dict::$defaultWidths["kerning-pairs"])) {
+                    $total -= Dict::$defaultWidths["kerning-pairs"][$pair];
+                }
+            }
+        }
+        return $total;
+    }
+
+
+
+    private function buildBadge(string $name,  int $covered, int $valid, float $limit, string $outputFile): void
+    {
+        $coverage = (($valid === 0) ? 100.0 : number_format(($covered * 100) / $valid, 1, '.')) . '%';
 
         $color = $coverage >= $limit ? '#4c1' : '#e54';
         $template = file_get_contents('template.svg');
+
+
+        $coverageLength = $this->calculateTextLength110($coverage);
+        $nameTextLength = $this->calculateTextLength110($name);
+        $rightWidth = ceil($coverageLength / 10) + 10;
+        $leftWidth = ceil($nameTextLength / 10) + 10;
+        $badgeWidth = $leftWidth + $rightWidth;
+        # The -10 below is for an exta buffer on right end of badge
+        $rightCenter = 10 * $leftWidth + $rightWidth * 5 - 10;
+        # The +10 below is for an extra buffer on left end of badge
+        $leftCenter = 10 + $leftWidth * 5;
+
+
+
+
+
+
         $template = str_replace('{{ name }}', $name, $template);
+        $template = str_replace('{{ nameTextLength }}', $nameTextLength, $template);
         $template = str_replace('{{ coverage }}', $coverage, $template);
+        $template = str_replace('{{ coverageLength }}', $coverageLength, $template);
         $template = str_replace('{{ color }}', $color, $template);
-        $template = str_replace('{{ width }}', $width, $template);
+        $template = str_replace('{{ rightWidth }}', $rightWidth, $template);
+        $template = str_replace('{{ badgeWidth }}', $badgeWidth, $template);
+        $template = str_replace('{{ rightCenter }}', $rightCenter, $template);
+        $template = str_replace('{{ leftWidth }}', $leftWidth, $template);
+        $template = str_replace('{{ leftCenter }}', $leftCenter, $template);
+
+
+
+
+
         file_put_contents($outputFile, $template);
     }
 
@@ -83,8 +141,6 @@ class Build
         $reportType = $options['report-type'] ?? 'clover';
         $coverageLineBadgeName = $options['coverage-line-badge-name'] ?? 'line coverage';
         $coverageBrancheBadgeName = $options['coverage-branche-badge-name'] ?? 'branche coverage';
-        $coverageLineBadgeWidth = intval($options['coverage-line-badge-width'] ?? 3000);
-        $coverageBrancheBadgeWidth = intval($options['coverage-branche-badge-width'] ?? 2000);
         $coverageLineBadgePath = $options['coverage-line-badge-path'] ?? 'coverage_line.svg';
         $coverageBrancheBadgePath = $options['coverage-branche-badge-path'] ?? 'coverage_breanche.svg';
         $coverageLinePercentOk = floatval($options['coverage-line-percent-ok'] ?? '80');
@@ -105,7 +161,6 @@ class Build
 
         $this->buildBadge(
             $coverageLineBadgeName,
-            $coverageLineBadgeWidth,
             $metric->linesCovered,
             $metric->linesValid,
             $coverageLinePercentOk,
@@ -114,7 +169,6 @@ class Build
 
         $this->buildBadge(
             $coverageBrancheBadgeName,
-            $coverageBrancheBadgeWidth,
             $metric->branchesCovered,
             $metric->branchesValid,
             $coverageBranchPercentOk,
